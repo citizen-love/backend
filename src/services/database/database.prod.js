ed84ecc6f64ab6138ad719dbd5b7ab76d9a21c1b
kondricz <kondricz@outlook.com>
2020-03-21 15:38:21 +0100

    location mapper provided to prod as well

--- src/services/database/database.prod.js
+++ src/services/database/database.prod.js
@@ -5,7 +5,11 @@ admin.initializeApp({});
 
 const database = admin.firestore();
 const incrementField = value => admin.firestore.FieldValue.increment(value);
+const getLocationEntry = (location) => {
+  const [lat, len] = location.split(',');
+  return new admin.firestore.GeoPoint(parseFloat(lat), parseFloat(len));
+};
 
 export default {
-  database, incrementField
+  database, incrementField, getLocationEntry
 };
