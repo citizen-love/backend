845dbd4bc7765f320f0414aa946b8468d04d9e8c
kondricz <kondricz@outlook.com>
2020-04-08 23:54:47 +0200

    development hotfixes

--- src/constants/versions.js
+++ src/constants/versions.js
@@ -2,13 +2,8 @@ import env from './environments';
 
 export default {
   http: {
-    version: '1.1',
+    version: '1.2',
     mode: env.ENVIRONMENT,
     type: 'HTTP'
-  },
-  dialogflow: {
-    version: '0.1',
-    mode: env.ENVIRONMENT,
-    type: 'DIALOGFLOW_WEBHOOK'
   }
 };
